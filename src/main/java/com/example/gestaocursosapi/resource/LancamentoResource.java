874ee211916e3117d1d11875e54e851070376547
9uilherme <9uilhermealencar@gmail.com>
2017-12-16 13:47:09 -0200

    lancamento update

--- src/main/java/com/example/gestaocursosapi/resource/LancamentoResource.java
+++ src/main/java/com/example/gestaocursosapi/resource/LancamentoResource.java
@@ -41,7 +41,7 @@ public class LancamentoResource {
 	@PostMapping("/filtro")
 	public ResponseEntity<List<Lancamento>> listarPorFiltro(final @RequestBody FiltroLancamento filtro, final HttpServletResponse response){
 		//List<Lancamento> lancamentos = lancamentoService.findByContaCodigoOrValor(filtro);
-		List<Lancamento> lancamentos = lancamentoService.findByContaCodigoOrDataBetweenOrValor(filtro);
+		List<Lancamento> lancamentos = lancamentoService.findByContaCodigoOrValor(filtro);
 		return !lancamentos.isEmpty() ? ResponseEntity.ok(lancamentos) : ResponseEntity.noContent().build();
 	}
 
